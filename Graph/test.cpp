6ab99b98dbc4b0edb233621720a5423859e2035d
lomot <lomot@qq.com>
2018-11-06 16:53:06 +0800

    graph

---
+++ Graph/test.cpp
@@ -0,0 +1,14 @@
+#include "../utils/function.h"
+#define MaxVertexNum 50
+typedef int WeightType;
+typedef struct GNode {
+  int Nv;
+  int Ne;
+  WeightType G[MaxVertexNum][MaxVertexNum];
+}GNode, *ptrToGNode;
+
+int main(int argc, char const *argv[])
+{
+  
+  return 0;
+}
